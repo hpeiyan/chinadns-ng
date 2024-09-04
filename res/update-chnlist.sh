4ecf73b7a567f07919ac392b11945bdb77a381a4
大雁 <huangbision@gmail.com>
2024-09-04 17:12:11 +0800

    Update update-chnlist.sh

--- res/update-chnlist.sh
+++ res/update-chnlist.sh
@@ -1,23 +1,27 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
 # exit if curl failed
 # url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
 # data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
 
 # echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
 
-#!/bin/bash
-set -o errexit
-set -o pipefail
+# Define temporary files
+tmp_domains_file=$(mktemp)
+tmp_json_file=$(mktemp)
 
 # exit if curl failed
 url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
-data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
+curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#' | awk -F/ '{print $2}' | sort | uniq > "$tmp_domains_file"
 
-# Extract domain, sort, remove duplicates, and format as JSON array
-domains=$(echo "$data" | awk -F/ '{print $2}' | sort | uniq | jq -R . | jq -s .)
+# Process domains and format them into JSON using jq
+jq -R . < "$tmp_domains_file" | jq -s . > "$tmp_json_file"
 
-# Create JSON structure
-json_output=$(jq -n --argjson domains "$domains" '{version: 1, rules: [{domain_suffix: $domains}]}')
+# Create the final JSON structure
+jq -n --slurpfile domains "$tmp_json_file" '{version: 1, rules: [{domain_suffix: $domains[0]}]}' > chnlist.json
 
-# Output JSON to file
-echo "$json_output" > chnlist.json
+# Clean up temporary files
+rm "$tmp_domains_file" "$tmp_json_file"
 
