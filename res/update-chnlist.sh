d50ee7a0d6cebf371a63b1c6e1fe55737bf73b2e
大雁 <huangbision@gmail.com>
2024-09-04 17:00:42 +0800

    Update update-chnlist.sh

--- res/update-chnlist.sh
+++ res/update-chnlist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -o errexit
 set -o pipefail
-set -x  # 开启调试模式
 
+# exit if curl failed
 url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
 data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
 
-echo "$data" | awk -F/ '{print $2}' | sort | uniq > chnlist.txt
+echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
