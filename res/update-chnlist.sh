de5032a35ec7ef8da827aa384cdddf252c26dbba
大雁 <huangbision@gmail.com>
2024-09-04 17:09:55 +0800

    Update update-chnlist.sh

--- res/update-chnlist.sh
+++ res/update-chnlist.sh
@@ -1,3 +1,9 @@
+# exit if curl failed
+# url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
+# data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
+
+# echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
+
 #!/bin/bash
 set -o errexit
 set -o pipefail
@@ -6,4 +12,12 @@ set -o pipefail
 url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
 data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
 
-echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
+# Extract domain, sort, remove duplicates, and format as JSON array
+domains=$(echo "$data" | awk -F/ '{print $2}' | sort | uniq | jq -R . | jq -s .)
+
+# Create JSON structure
+json_output=$(jq -n --argjson domains "$domains" '{version: 1, rules: [{domain_suffix: $domains}]}')
+
+# Output JSON to file
+echo "$json_output" > chnlist.json
+
