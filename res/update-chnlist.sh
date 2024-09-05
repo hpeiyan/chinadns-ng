58b016617fe7563fb71491e6bb5a12748bfe0c01
大雁 <huangbision@gmail.com>
2024-09-05 11:07:09 +0800

    Update update-chnlist.sh

--- res/update-chnlist.sh
+++ res/update-chnlist.sh
@@ -2,26 +2,31 @@
 set -o errexit
 set -o pipefail
 
-# exit if curl failed
-# url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
-# data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
-
-# echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
-
 # Define temporary files
 tmp_domains_file=$(mktemp)
+tmp_adblock_file=$(mktemp)
 tmp_json_file=$(mktemp)
+tmp_adblock_json_file=$(mktemp)
 
-# exit if curl failed
+# 获取加速的中国域名列表并处理
 url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
 curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#' | awk -F/ '{print $2}' | sort | uniq > "$tmp_domains_file"
 
-# Process domains and format them into JSON using jq
+# 从 adblockdns.txt 获取域名并处理
+adblock_url='https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt'
+curl -4fsSkL "$adblock_url" | grep '^||.*\^$' | sed 's/^||//;s/\^$//' | sort | uniq > "$tmp_adblock_file"
+
+# 将中国域名列表转化为 JSON
 jq -R . < "$tmp_domains_file" | jq -s . > "$tmp_json_file"
 
-# Create the final JSON structure
+# 将 adblock 列表转化为 JSON
+jq -R . < "$tmp_adblock_file" | jq -s . > "$tmp_adblock_json_file"
+
+# 创建 chnlist.json 的 JSON 结构
 jq -n --slurpfile domains "$tmp_json_file" '{version: 1, rules: [{domain_suffix: $domains[0]}]}' > chnlist.json
 
-# Clean up temporary files
-rm "$tmp_domains_file" "$tmp_json_file"
+# 创建 adblock.json 的 JSON 结构
+jq -n --slurpfile adblock_domains "$tmp_adblock_json_file" '{version: 1, rules: [{domain_suffix: $adblock_domains[0]}]}' > adblock.json
 
+# 清理临时文件
+rm "$tmp_domains_file" "$tmp_adblock_file" "$tmp_json_file" "$tmp_adblock_json_file"
